1547e2a3d199f2ac026cbd1784feccbc54f5a1db
jonathanyu1 <jonathanyusir@gmail.com>
2021-01-20 17:29:54 -0500

    added html skeleton, added basic calculator functions and operate function

---
+++ myscript.js
@@ -0,0 +1,42 @@
+let result = 0;
+
+// basic operations - add, subtract, multiply, divide
+
+function addNum(num1, num2){
+    return num1+num2;
+}
+
+function subtractNum(num1,num2){
+    return num1-num2;
+}
+
+function multiplyNum(num1,num2){
+    return num1*num2;
+}
+
+function divideNum(num1,num2){
+    return num1/num2;
+}
+
+// takes operator and 2 numbers, calls corresponding function with 2 numbers
+
+function operate(num1,num2,operator){
+    switch (operator){
+        case 'add':
+            result = addNum(num1,num2);
+            console.log(result);
+            break;
+        case 'subtract':
+            result = subtractNum(num1,num2);
+            console.log(result);
+            break;
+        case 'multiply':
+            result = multiplyNum(num1,num2);
+            console.log(result);
+            break;
+        case 'divide':
+            result = divideNum(num1,num2);
+            console.log(result);
+            break;
+    }
+}
